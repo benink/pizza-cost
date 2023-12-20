eaaefb1adae1a315226d13e2617667404aa81914
benink <117493347+benink@users.noreply.github.com>
2023-12-20 10:53:47 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+let HST = 0.13
+let shop_rent = 1
+let material_cost = 0.5
+let labour_cost = 0.75
+let subtotal = labour_cost + shop_rent + material_cost * 7
+let Tax = subtotal * HST
+let Total = subtotal + Tax
